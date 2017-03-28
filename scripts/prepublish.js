968939ef02c05a23464ed485f1ca4f78612d0cd7
Stéphane MBAPE <smbape@yahoo.fr>
2017-03-28 20:38:37 +0200

    use local coffee script for generation

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,38 +1,9 @@
-// jshint node: true
-'use strict';
+var sysPath = require("path");
+var rimraf = require("rimraf");
+rimraf(sysPath.join(__dirname, "../lib"), function(err) {
+    if (err) {
+        throw err;
+    }
 
-var coffeescript = require('coffee-script'),
-    fs = require('fs'),
-    sysPath = require('path'),
-    mkdirp = require('mkdirp'),
-    explore = require('fs-explorer').explore,
-    rimraf = require('rimraf'),
-    src = sysPath.join(__dirname, '..', 'src'),
-    lib = sysPath.join(__dirname, '..', 'lib');
-
-rimraf(lib, function(err) {
-    if (err) throw err;
-    explore(src, function(path, stats, next) {
-        var dst = sysPath.join(lib, sysPath.relative(src, path));
-        mkdirp(sysPath.dirname(dst), function(err) {
-            var readable, writable;
-            if (err) return next(err);
-            if (/\.coffee$/.test(path)) {
-                fs.readFile(path, function(err, data) {
-                    if (err) return next(err);
-                    var compiled = coffeescript.compile(data.toString(), {
-                        bare: true
-                    });
-                    fs.writeFile(dst.replace(/\.coffee$/, '.js'), compiled, next);
-                });
-            } else {
-                readable = fs.createReadStream(path);
-                writable = fs.createWriteStream(dst);
-                readable.pipe(writable);
-                writable.on('finish', next);
-            }
-        });
-    }, function(err) {
-        if (err) throw err;
-    });
+    require("coffee-script/bin/coffee");
 });
